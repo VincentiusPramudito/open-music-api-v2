3d05aa1ad48914e457e23c285c1242b54af8888d
VincentiusPramudito <vpramudito@yahoo.com>
2025-06-08 20:02:12 +0700

    initial commit open music v2

---
+++ src/server.js
@@ -0,0 +1,15 @@
+
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST
+  });
+
+  await server.start();
+  console.log(`App is running on ${server.info.uri}`);
+};
+
+init();
